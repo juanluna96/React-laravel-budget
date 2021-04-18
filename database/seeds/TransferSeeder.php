f733a1e758d592e4b3aab8612db7975d5bbdb081
juanluna <juandluna17@gmail.com>
2021-04-18 16:09:12 -0500

    Creando las migraciones

--- database/seeds/TransferSeeder.php
+++ database/seeds/TransferSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class TransferSeeder extends Seeder
 {
@@ -11,6 +13,19 @@ class TransferSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('transfers')->insert([
+            [
+                'description' => 'Salary', 'amount' => '4800', 'wallet_id' => 1, 'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'description' => 'Salary', 'amount' => '4800', 'wallet_id' => 1, 'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'description' => 'Salary', 'amount' => '4800', 'wallet_id' => 1, 'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+        ]);
     }
 }
--- database/seeds/WalletSeeder.php
+++ database/seeds/WalletSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Support\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class WalletSeeder extends Seeder
 {
@@ -11,6 +13,10 @@ class WalletSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('wallets')->insert([
+            'money' => 3600.00,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
     }
 }
